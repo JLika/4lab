1f1859f1ff67e71c0de9a1a82a85fca3e246798d
Lika <p3rlaur@gmail.com>
2021-09-15 15:50:58 +0700

    add f del

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -23,5 +23,11 @@ namespace ConsoleApp1
 
 
         }
+
+
+        public void del(int a, int b)
+        {
+            Console.WriteLine($" Результат: {a} / {b} = " + (a / b));
+        }
     }
 }
