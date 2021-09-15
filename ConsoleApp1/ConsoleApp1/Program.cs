0c52d3f44aae19b51adc98ae294f86e259e81697
Lika <p3rlaur@gmail.com>
2021-09-15 15:43:55 +0700

    add a,b,k

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            int a = 0, b = 0;
+            string k;
+
+            Console.WriteLine("Введите a:");
+            Console.WriteLine();
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите b:");
+            Console.WriteLine();
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите знак:");
+            Console.WriteLine();
+            k = Convert.ToString(Console.ReadLine());
+
+
         }
     }
 }
